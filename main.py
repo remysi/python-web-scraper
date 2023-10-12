9cf0ba6e8a5ba91e752270ff2ef3258c688e3e6c
Remysi <remy.silanto99@gmail.com>
2023-10-12 15:44:17 +0300

    Scraper which searches a given url and scrapes every text out of the page and pushes it all out in a .txt file constructed.

--- main.py
+++ main.py
@@ -17,37 +17,63 @@ from selenium.webdriver.common.by import By
 # FirefoxDriver = webdriver.Firefox()
 
 
+def give_all_text_to_ai(text):
+    with open("pageText.txt", "w") as file:
+        file.write(text)
+
+
+'''
+def give_all_links_to_ai(link):
+    with open("pageLinks.txt", "w") as file:
+        file.write(link)
+'''
+
+
 def scrape(site):
-
     reqs = requests.get(site)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
 
-    soup = BeautifulSoup(reqs.text,'html.parser')
+    all_text = soup.get_text()
 
+    print("All text: ", all_text)
+
+    for script in soup(["script", "style"]):
+        script.decompose()
+
+    all_text_without_script = soup.get_text()
+    print("All text without script", all_text_without_script)
+
+    give_all_text_to_ai(all_text_without_script)
+
+    '''
+    # Gets all links of the given URL and loops through every one of them
     for i in soup.find_all("a"):
 
-        # Check if 'href' attribute exists otherwise crashes
+        # Check if 'href' attribute exists to avoid crashes
         if 'href' in i.attrs:
             href = i.get('href')
 
             if href.startswith("/"):
-
-                # Ignores different localization links
-                if "/fi/" in href or "/sv/" in href or "/en/" in href or "/de/" in href:
-                    continue
-
+                
+                # Creates the url path
                 full_url = urljoin(site, href)
 
+                # Adds the handled url to list of urls
                 if full_url not in urls:
                     urls.append(full_url)
-                    print("Vierailtu sivulla: ", full_url)
-                    print("Kaikki sivut missä käyty: ", urls)
-                    time.sleep(0.5)
-                    scrape(full_url)
+
+    print("All links of the page: ", urls)
+    '''
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+def main(url):
 
     # URL to be scraped
-    site = "https://www.helen.fi/"
-    urls = []
-    scrape(site)
+    # site = url
+    # site = "https://www.helen.fi/"
+    # urls = []
+    scrape(url)
+
+
+main("https://www.helen.fi/")
